e14fa004741573390758923b73ea7c1a5624093b
Ernawati Hutasoit <ernabelajar9@gmail.com>
2026-04-17 11:10:54 +0700

    update judul di file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Kelola Duitku",
+      title: "Pencatatan Keuangan Pribadi",
       // TEMA PINK
       theme: ThemeData(
         primarySwatch: Colors.pink,
